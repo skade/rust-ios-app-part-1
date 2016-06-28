b3298b757785a6824a9244ddbfff1b312280d12f
Florian Gilcher <florian.gilcher@asquera.de>
2016-06-28 09:53:37 +0200

    Rename println to print (new Swift syntax)

--- ios/RustHelloWorld/RustHelloWorld/AppDelegate.swift
+++ ios/RustHelloWorld/RustHelloWorld/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let result = rust_hello_world()
-        println("I called Rust and got \(result)")
+        print("I called Rust and got \(result)")
         return true
     }
 
